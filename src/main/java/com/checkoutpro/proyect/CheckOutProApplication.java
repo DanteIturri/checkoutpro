fbb5e65bd36a1df7fb330fd27a3ad610d20414ef
Dante <danteiturri@gmail.com>
2022-05-21 18:43:24 -0300

    Initial commit

---
+++ src/main/java/com/checkoutpro/proyect/CheckOutProApplication.java
@@ -0,0 +1,13 @@
+package com.checkoutpro.proyect;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheckOutProApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheckOutProApplication.class, args);
+	}
+
+}
